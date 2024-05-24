966741b4aa7c2703ad3d84050db92000faabfb17
John-Rashta <john_rashta@outlook.com>
2024-05-24 17:40:42 +0100

    Finish Step 2 to 4
    Forgot to commit, finished steps 2 to 4, added comments everytime, trying to kinda
    plan it out/pseudocode it a bit, there are probably better solutions for step 3
    but i like returning the function itself so that it calls again if user is wrong, initially
    just called the function, got undefined than realized i needed to return the function call,
    not just call it

--- game.js
+++ game.js
@@ -1,1 +1,48 @@
 console.log("Hello World");
+
+
+// write a function that returns the computer choice- rock, paper or scissors
+
+// CREATE function to randomly choose computer choice
+function getComputerChoice() {
+    // GET random number between 0 and 2 with math.random
+    let choice = Math.floor(Math.random() * 3);
+    // IF number is 0, return rock
+    if (choice === 0) {
+        return "rock";
+
+    //  IF number is 1, return paper
+    } else if (choice === 1) {
+        return "paper";
+
+    //  IF number is 2, return scissor
+    } else {
+        return "scissor";
+    }
+}
+
+
+//CREATE function to validate user choice
+function getHumanChoice() {
+//GET user choice to play
+let userChoice = prompt("What's is your choice?");
+//MODIFY the user input by lowercasing it
+userChoice = userChoice.toLowerCase();
+//VALIDATE the user choice- has to be rock/paper/scissor
+if (userChoice === "paper" || userChoice === "rock" || userChoice === "scissor")
+    {
+        //IF valid return the choice
+        return userChoice;
+
+    // ELSE repeat the process
+    } else {
+       return getHumanChoice();
+
+    }
+}
+
+
+//CREATE 2 variables for each player score
+//AND initialize them to 0
+let humanScore = 0;
+let computerScore = 0;
